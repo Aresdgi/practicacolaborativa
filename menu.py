46710997ebc25884dc85a8171177200b58a7fb4c
aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-11-26 12:39:09 +0100

    agregados cambios a la reama menu

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from main import sumar, restar, multiplicar
+
 def mostrar_menu():
     while True:  # El ciclo continuará hasta que el usuario elija salir
         print("Seleccione una opción:")
@@ -20,9 +22,26 @@ def mostrar_menu():
             print("Saliendo del programa...")
             break
 
-        # Si la opción no es salir, solicitar un número
+        # Si la opción no es salir, solicitar dos números
         try:
-            num = float(input("Ingrese un número: "))
+            num1 = float(input("Ingrese el primer número: "))
+            num2 = float(input("Ingrese el segundo número: "))
         except ValueError:
             print("Entrada no válida. Debe ingresar un número.")
-            continue  # Si la entrada no es un número, vuelve a mostrar el menú
+            continue
+
+        # Realizar la operación seleccionada
+        try:
+            if opcion == '1':
+                resultado = sumar(num1, num2)
+            elif opcion == '2':
+                resultado = restar(num1, num2)
+            elif opcion == '3':
+                resultado = multiplicar(num1, num2)
+            else:
+                print("Opción no implementada.")
+                continue
+
+            print(f"El resultado es: {resultado}")
+        except ValueError as e:
+            print(e)
